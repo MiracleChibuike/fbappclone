480f36370426481cdf6d8bf6fd84fbe91b1557df
MiracleChibuike <miraclechibuike27@gmail.com>
2023-08-09 19:02:46 +0100

    appVersion enabled

--- js/index.js
+++ js/index.js
@@ -1,2 +1,18 @@
  let phoneCheck = document.getElementById("checkversion");
- phoneCheck.innerHTML = navigator.appVersion;
+ const userDevice = navigator.appVersion;
+ phoneCheck.textContent = userDevice;
+
+//  const userAgent = navigator.userAgent;
+//  const deviceName = {
+//     ipad: /ipad/.test(userAgent),
+//     iphone: /iphone/.test(userAgent),
+//     Android: /Android/.test(userAgent)
+//  }
+//  if (deviceName.ipad) {
+//     phoneCheck.textContent = `You are ${ipad} on this device`
+//  }else if(deviceName.iphone){
+//     phoneCheck.textContent = iphone.test(userAgent)
+//  }
+//  else if(deviceName.Android){
+//     phoneCheck.textContent = Android.test(userAgent)
+//  }
